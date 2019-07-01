6ec0adca33219e0a0f3bca3a088e1d5b2889d365
Jeppe Vennekilde <jeppe@jeppes.info>
2019-07-01 03:14:36 +0200

    Fixed incorrect port

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect -f
 
-spawn ssh -4 -D 12354 -o StrictHostKeyChecking=no $::env(ssh_user)@$::env(ssh_host)
+spawn ssh -4 -D 12345 -o StrictHostKeyChecking=no $::env(ssh_user)@$::env(ssh_host)
 expect "*ssword: "
 send $::env(ssh_pass)\n
 interact
