6af3b4bacebcfb66086c2852489bc778af6ee9f1
Jeppe Vennekilde <jeppe@jeppes.info>
2019-07-01 02:50:52 +0200

    Fixed incorrect spelling

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
 
-spawn ssh -D 12354 -o StrictHostChecking=no -l -q -C -N $::env(ssh_user)@$::env(ssh_host)
+spawn ssh -D 12354 -o StrictHostKeyChecking=no -l -q -C -N $::env(ssh_user)@$::env(ssh_host)
 expect "*ssword: "
 send $::env(ssh_pass)
